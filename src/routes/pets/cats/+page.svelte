65610bb8c5e2843c41461b057974605e100d976d
Avery Aaron <sylvanserene@gmail.com>
2024-07-13 23:18:54 +0300

    Added temp pages

---
+++ src/routes/pets/cats/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Nav from '../../../Nav.svelte';
+	import Footer from '../../../Footer.svelte';
+</script>
+
+<Nav />
+
+<main>
+	<h1>Cats</h1>
+</main>
+
+<Footer />
---
+++ src/routes/schedule-consultation/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Nav from '../../Nav.svelte';
+	import Footer from '../../Footer.svelte';
+</script>
+
+<Nav />
+
+<main>
+	<h1>Schedule Consultation</h1>
+</main>
+
+<Footer />
